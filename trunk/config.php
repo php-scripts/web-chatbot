9bad47e7635074cb53abd4ed195a926cf6f2d3d7
dbliss3@gmail.com <dbliss3@gmail.com@836ed15e-56b3-5678-ffd8-2fa29726dcbf>
2014-10-27 14:58:12 +0000

    new file

---
+++ trunk/config.php
@@ -0,0 +1,11 @@
+<?php
+// config
+$cfg["answersfile"] = "data/answers.csv";
+$cfg["scrolling"] = true;
+$cfg["user"] = "admin";
+$cfg["pass"] = "admin";
+$cfg["encoding"] = "UTF-8";
+$cfg["wsite_ul"] = "http://nasa.fr/webchat/";
+$cfg["wsite_vs"] = "Chatbot v1.2.1";
+$cfg["wsite_ct"] = "masrourmouad@yahoo.fr";
+
